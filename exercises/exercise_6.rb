db32b4932638676a5569d3d4a05fda6efa1717d2
Aaron <aaron3993@hotmail.com>
2020-07-21 03:20:56 +0000

    Added error messages

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+Store.has_many :employees
+Employee.belongs_to :store
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Aaron", last_name: "Sham", hourly_rate: 100)
+@store1.employees.create(first_name: "Jenny", last_name: "Jung", hourly_rate: 80)
+@store1.employees.create(first_name: "Ned", last_name: "Stark", hourly_rate: 200)
+@store2.employees.create(first_name: "Jaime", last_name: "Lannister", hourly_rate: 300)
+@store2.employees.create(first_name: "Dhaenarys", last_name: "Targaryen", hourly_rate: 250)
+@store2.employees.create(first_name: "Oberon", last_name: "Martell", hourly_rate: 150)
+@store2.employees.create(first_name: "Theon", last_name: "Greyjoy", hourly_rate: 0)
