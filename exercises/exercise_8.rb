d5140319d5b54536ce4d5c1ad6e69616ea261e38
Aaron <aaron3993@hotmail.com>
2020-07-25 20:40:34 +0000

    Finished exercise 8 and removed comments from 7

---
+++ exercises/exercise_8.rb
@@ -0,0 +1,23 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+require_relative './exercise_5'
+require_relative './exercise_6'
+require_relative './exercise_7'
+
+puts "Exercise 8"
+puts "----------"
+
+class Employee
+  after_create :set_password
+
+  private
+    def set_password
+      self.password = "password"
+    end
+end
+
+tyrion = @store1.employees.create(first_name: "Tyrion", last_name: "Lannister", hourly_rate: 60)
+puts tyrion.password
